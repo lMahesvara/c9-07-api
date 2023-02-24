412497ff4811d5f9f67c04cdbf831d5f373dfb0e
Erick Bernal Amparano <erick082016@gmail.com>
2023-02-24 09:06:49 -0700

    change db uri

--- src/config.js
+++ src/config.js
@@ -1,12 +1,14 @@
-import * as dotenv from "dotenv"
+import * as dotenv from 'dotenv'
 import { fileURLToPath } from 'url'
 import path from 'path'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-dotenv.config({ path:__dirname + '/.env' })
+dotenv.config({ path: __dirname + '/.env' })
 
-export const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://robert:04hfDfLOndHtcgd0@cluster0.npkfkpc.mongodb.net/?retryWrites=true&w=majority"
+export const MONGODB_URI =
+  process.env.MONGODB_URI ||
+  'mongodb+srv://admin:2OCpWXornrvcp8fc@cluster0.pe9ii90.mongodb.net/Cluster0?retryWrites=true&w=majority'
 export const PORT = process.env.PORT || 5000
-export const KEYSECRETTOKEN = process.env.KEYSECRETTOKEN  || "NOCOUNTRY-PROYECT"
+export const KEYSECRETTOKEN = process.env.KEYSECRETTOKEN || 'NOCOUNTRY-PROYECT'
--- src/database/conection.js
+++ src/database/conection.js
@@ -1,16 +1,16 @@
-import mongoose from "mongoose"
-import { MONGODB_URI } from "../config.js"
-
+import mongoose from 'mongoose'
+import { MONGODB_URI } from '../config.js'
 
 export const connectDB = async () => {
-    try {
-        mongoose.set("strictQuery", false);
-        mongoose.connect(MONGODB_URI, {
-            
-        }).then(() => {
-            console.log('Conectado a la base de datos.')
-        })
-    } catch (error) {
-        console.log(error)
-    }
+  try {
+    mongoose.set('strictQuery', false)
+    mongoose
+      .connect(MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then(() => console.log('Database connected'))
+  } catch (error) {
+    console.log(error)
+  }
 }
