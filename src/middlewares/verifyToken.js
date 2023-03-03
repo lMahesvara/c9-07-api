a0af93162e0b4a7ad8b29ff74cae2af31e4181f3
Erick Bernal Amparano <erick082016@gmail.com>
2023-03-03 11:36:26 -0700

    fix

--- src/middlewares/verifyToken.js
+++ src/middlewares/verifyToken.js
@@ -1,29 +1,28 @@
-import jwt from "jsonwebtoken";
-import { KEYSECRETTOKEN } from "../config.js";
+import jwt from 'jsonwebtoken'
+import { KEYSECRETTOKEN } from '../config.js'
 
 export const authAdmin = (req, res) => {
-  
-  let token = req.headers["authorization"]
+  let token = req.headers['authorization']
 
   if (!token) {
-    return res.status(401).send({ message: "No token provided!" });
+    return res.status(401).send({ error: true, msg: 'No token provided!' })
   }
 
   if (token.startsWith('Bearer ')) {
     // Remove Bearer from string
-    token = token.slice(7, token.length).trimLeft();
+    token = token.slice(7, token.length).trimLeft()
   }
 
-  jwt.verify(token, KEYSECRETTOKEN , (err, decoded) => {
+  jwt.verify(token, KEYSECRETTOKEN, (err, decoded) => {
     if (err) {
-      if(err.expiredAt){
-        return res.status(401).send({error: true, msg: "Token expirado" })
+      if (err.expiredAt) {
+        return res.status(401).send({ error: true, msg: 'Token expirado' })
       } else {
-        return res.status(401).send({error: true, msg: "No estas autorizado" })
+        return res.status(401).send({ error: true, msg: 'No estas autorizado' })
       }
     }
 
     const username = decoded.user
-    return res.json({error: false, msg: {token, username} })
-  });
+    return res.json({ error: false, msg: { token, username } })
+  })
 }
